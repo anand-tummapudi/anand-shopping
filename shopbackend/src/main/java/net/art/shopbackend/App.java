5716209b7fa93604d8c3dbdb3bea2af37d1f562e
anandaraju_t <anandaraju_t@edgeverve.com>
2019-02-03 08:06:53 +0530

    Adding the backend project

---
+++ shopbackend/src/main/java/net/art/shopbackend/App.java
@@ -0,0 +1,13 @@
+package net.art.shopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
